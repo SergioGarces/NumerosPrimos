1ab856a14360c8bed010e7dabf09a25d2801f194
alumno <alumno@aula01-07>
2016-10-24 19:46:25 +0200

    Ejercicio Terminado

--- NumerosPrimos/src/numerosprimos/NumerosPrimos.java
+++ NumerosPrimos/src/numerosprimos/NumerosPrimos.java
@@ -5,17 +5,71 @@
  */
 package numerosprimos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
  */
 public class NumerosPrimos {
 
-    /**
-     * @param args the command line arguments
-     */
+    //Devuelve true si es primo, false si no lo es
+    public static boolean CompruebaPrimo(int num){
+        boolean primo = true;
+        
+        for(int i=2; i<num; i++){
+            if ( (num % i)==0 ) {
+                primo = false;
+                i=num+1;
+            }
+            
+            
+        }
+        
+        return primo;
+    }
+    
+    public static int SiguientePrimo(int num){
+        int primo = 0;
+        boolean bandera = true;
+        
+        for(int i = (num+1); bandera == true; i++){
+            if (CompruebaPrimo(i)){
+                bandera= false;
+                primo = i;
+            }
+        }
+        return primo;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+       boolean bandera = true;
+       int num = 0;
+       Scanner leer = new Scanner(System.in);
+       
+       while (bandera){
+           System.out.println("Dime un numero por teclado para comprobar si es primo");
+            num = leer.nextInt();
+            
+            if (CompruebaPrimo(num)){
+                System.out.println("El numero " + num + " es primo" );
+                System.out.println("El siguiente primo es " + SiguientePrimo(num) );
+            }else{
+                System.out.println("El numero " + num + " no es primo" );
+                System.out.println("El siguiente primo es " + SiguientePrimo(num) );
+                    }
+            
+            if (num == 0){
+                bandera= false;
+                        
+            }
+       
+       }
+       
+       
+        
+        
     }
     
 }
